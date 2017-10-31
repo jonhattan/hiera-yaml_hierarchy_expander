17b4f474350280514401a2f8e4ee1dba71a96eda
Jonathan Araña Cruz <jonhattan@faita.net>
2017-10-31 09:49:26 +0100

    Cache hierarchy.

--- lib/hiera/backend/yaml_hierarchy_expander_backend.rb
+++ lib/hiera/backend/yaml_hierarchy_expander_backend.rb
@@ -6,6 +6,7 @@ class Hiera
         Hiera.debug("Hiera YAML_hierarchy_expander backend starting")
 
         @cache = cache || Filecache.new
+        @hierarchy = nil
       end
 
       def lookup(key, scope, order_override, resolution_type)
@@ -13,6 +14,47 @@ class Hiera
 
         Hiera.debug("Looking up #{key} in YAML_hierarchy_expander backend")
 
+        @hierarchy ||= get_hierarchy(scope, order_override)
+
+        Backend.datasourcefiles(:yaml_hierarchy_expander, scope, "yaml", order_override, hierarchy) do |source, yamlfile|
+          data = @cache.read_file(yamlfile, Hash) do |data|
+            YAML.load(data) || {}
+          end
+
+          next if data.empty?
+          next unless data.include?(key)
+
+          # Extra logging that we found the key. This can be outputted
+          # multiple times if the resolution type is array or hash but that
+          # should be expected as the logging will then tell the user ALL the
+          # places where the key is found.
+          Hiera.debug("Found #{key} in #{source}")
+
+          # for array resolution we just append to the array whatever
+          # we find, we then goes onto the next file and keep adding to
+          # the array
+          #
+          # for priority searches we break after the first found data item
+          new_answer = Backend.parse_answer(data[key], scope)
+          case resolution_type
+          when :array
+            raise Exception, "Hiera type mismatch: expected Array and got #{new_answer.class}" unless new_answer.kind_of? Array or new_answer.kind_of? String
+            answer ||= []
+            answer << new_answer
+          when :hash
+            raise Exception, "Hiera type mismatch: expected Hash and got #{new_answer.class}" unless new_answer.kind_of? Hash
+            answer ||= {}
+            answer = Backend.merge_answer(new_answer,answer)
+          else
+            answer = new_answer
+            break
+          end
+        end
+
+        return answer
+      end
+
+      def get_hierarchy(scope, order_override):
         # Prepare expanders.
         expanders = {}
         if Config[:yaml_hierarchy_expander] && Config[:yaml_hierarchy_expander][:expanders]
@@ -62,42 +104,7 @@ class Hiera
         end
         hierarchy = h2.flatten
 
-        Backend.datasourcefiles(:yaml_hierarchy_expander, scope, "yaml", order_override, hierarchy) do |source, yamlfile|
-          data = @cache.read_file(yamlfile, Hash) do |data|
-            YAML.load(data) || {}
-          end
-
-          next if data.empty?
-          next unless data.include?(key)
-
-          # Extra logging that we found the key. This can be outputted
-          # multiple times if the resolution type is array or hash but that
-          # should be expected as the logging will then tell the user ALL the
-          # places where the key is found.
-          Hiera.debug("Found #{key} in #{source}")
-
-          # for array resolution we just append to the array whatever
-          # we find, we then goes onto the next file and keep adding to
-          # the array
-          #
-          # for priority searches we break after the first found data item
-          new_answer = Backend.parse_answer(data[key], scope)
-          case resolution_type
-          when :array
-            raise Exception, "Hiera type mismatch: expected Array and got #{new_answer.class}" unless new_answer.kind_of? Array or new_answer.kind_of? String
-            answer ||= []
-            answer << new_answer
-          when :hash
-            raise Exception, "Hiera type mismatch: expected Hash and got #{new_answer.class}" unless new_answer.kind_of? Hash
-            answer ||= {}
-            answer = Backend.merge_answer(new_answer,answer)
-          else
-            answer = new_answer
-            break
-          end
-        end
-
-        return answer
+        return hierarchy
       end
 
       private
